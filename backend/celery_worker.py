44e486e65d53ec2a4787f3968f24b7aa130fa6d0
Harsha30012005 <harshadhardhavardhan@gmail.com>
2026-06-27 10:38:51 +0530

    commit

---
+++ backend/celery_worker.py
@@ -0,0 +1,17 @@
+from celery import Celery
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+celery_app = Celery(
+    "worker",
+    broker=os.getenv("CELERY_BROKER_URL"),
+    backend=os.getenv("CELERY_RESULT_BACKEND")
+)
+
+celery_app.conf.update(
+    task_serializer="json",
+    result_serializer="json",
+    accept_content=["json"],
+)
