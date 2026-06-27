51b615e05b0e781d0a5ad2164be60074fad238e7
Harsha30012005 <harshadhardhavardhan@gmail.com>
2026-06-27 10:24:02 +0530

    commit all

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from routes.campaign import router
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI(title="MultiModal AI Content Engine")
 
@@ -7,4 +11,8 @@ app.include_router(router)
 
 @app.get("/")
 def root():
-    return {"message": "MultiModal AI Content Engine is running!"}
+    redis_url = os.getenv("REDIS_URL")
+    return {
+        "message": "MultiModal AI Content Engine is running!",
+        "redis_url": redis_url
+    }
